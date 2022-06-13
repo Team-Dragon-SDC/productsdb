d5f9121b7b9b3b42c658fb02fbab9ff7f9e03f21
Amanda <amandaqwright@gmail.com>
2022-06-13 09:20:07 -0500

    starting to write queries

---
+++ server/pool.js
@@ -0,0 +1,9 @@
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//   user: 'amandawright',
+//   host: 'localhost',
+//   database: 'dragon',
+//   password: '',
+//   port: 5432,
+// })
+// modules.exports = pool
